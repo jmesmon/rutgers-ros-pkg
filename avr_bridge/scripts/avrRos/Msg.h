bb7dc9bddd59638f2454d7d5ffe9e285ba9c8b42
John Mesmon <jmesmon@gmail.com>
2010-11-20 16:02:49 -0500

    avrRos: make Msg pure virtual.

--- avr_bridge/scripts/avrRos/Msg.h
+++ avr_bridge/scripts/avrRos/Msg.h
@@ -10,21 +10,24 @@
 
 #include <stdint.h>
 
-
 class Msg {
 public:
-	Msg();
-	Msg(uint8_t* data);
-	virtual uint16_t serialize(uint8_t * outbuffer);
-	virtual uint16_t deserialize(uint8_t * data);
-	virtual uint16_t bytes();
-	 ~Msg();
-private:
-	char * _buffer;
-	int _length; //length of byte stream;
+	virtual Msg() = 0;
+	virtual Msg(uint8_t* data) = 0;
 
+	/* returns the length of the serialized data.
+	 * XXX: UNSAFE: outbuffer cannot be not length checked
+	 */
+	virtual uint16_t serialize(uint8_t * outbuffer) = 0;
+
+	/* ???
+	 * How is length of data indicated?
+	 */
+	virtual uint16_t deserialize(uint8_t * data) = 0;
+
+
+	virtual uint16_t bytes() = 0;
+	virtual ~Msg() = 0;
 };
-
-
 
 #endif /* MSG_H_ */

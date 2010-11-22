69cd6840ec5814d5babb625d65e26aa28ec5677f
Cody Schafer <cpschafer@gmail.com>
2010-11-22 16:56:43 -0500

    avrRos: some vector.h cleanup.

--- avr_bridge/scripts/avrRos/vector.h
+++ avr_bridge/scripts/avrRos/vector.h
@@ -11,77 +11,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-namespace ROS{
+namespace ROS {
 
-template<class T> class vector{
+/* XXX: This is poorly named: the ros documentation refers to them as "Fixed
+ * or variable sized arrays (lists)". Additionally, there is no reason to use
+ * the same class for both Fixed and Variable sized, in fact it makes it
+ * rather confusing in dealing with the serialize/deserialize methods.
+ */
+template<class T> class vector {
 public:
 
-	vector(){};
-	vector(uint16_t size){fixed = 1;  setMaxLength(size); length=size;};
-	uint16_t size(); //
-	T& operator[] (int i) { return data[i];}
-	uint16_t serialize(uint8_t * buffer);
-	uint16_t deserialize(uint8_t * buffer);
-	uint16_t bytes();// output size to store in bytes
+	vector(void)
+	{}
 
-	void push_back(T item){
-		if (length <maxLength) {
-		data[length] = item;
-		length++;
-				}
+	vector(uint16_t size)
+	{
+		fixed = 1;
+		setMaxLength(size);
+		length = size;
 	}
 
-	T pop_back(){length--; return data[length+1];};
+	void setMaxLength(uint16_t L)
+	{
+		max_length = L;
+		data = malloc( L* sizeof(T));
+	}
 
-	~vector(){free(data);}
+	T& operator[] (int i)
+	{
+		return data[i];
+	}
+
+	void push_back(T item)
+	{
+		if (length < max_length) {
+			data[length] = item;
+			length++;
+		}
+	}
+
+	T pop_back()
+	{
+		length--;
+		return data[length+1];
+	}
+
+	~vector()
+	{
+		free(data);
+	}
+
+	uint16_t serialize(uint8_t * buffer);
+	uint16_t deserialize(uint8_t * buffer);
+
+	/* FIXME: choose better name. 'Size' vs 'Bytes' */
+	uint16_t bytes(void)// output size to store in bytes
+	{
+		return length*sizeof(T);
+	}
+
+	uint16_t size(void)
+	{
+		return length;
+	}
+
+	/* FIXME: should this be public? */
 	bool fixed;
-
-	void setMaxLength(uint16_t L){ maxLength = L;
-								data = (T*)malloc( L* sizeof(T));};
-
 private:
 	T* data;
 	uint16_t length;
-	uint16_t maxLength;
-	void setSize(uint16_t size){ length = size;};
+	uint16_t max_length;
+
+	void setSize(uint16_t size)
+	{
+		length = size;
+	}
+
 	uint16_t serializeFixed(uint8_t * buffer);
 	uint16_t serializeVariable(uint8_t* buffer);
 	uint16_t deserializeFixed(uint8_t *buffer);
 	uint16_t deserializeVariable(uint8_t * buffer);
 };
 
-
-
 template<class T>
-uint16_t vector<T>::size(){
-	return length;
-}
-template<class T>
-uint16_t vector<T>::bytes(){
-	return length*sizeof(T);
-}
-
-template<class T>
-uint16_t vector<T>::serializeFixed(uint8_t * buffer){
+uint16_t vector<T>::serializeFixed(uint8_t * buffer)
+{
 	memcpy(buffer,data, this->bytes());
 	return this->bytes();
 }
 
 
 template<class T>
-uint16_t vector<T>::serializeVariable(uint8_t * buffer){
+uint16_t vector<T>::serializeVariable(uint8_t * buffer)
+{
 	memcpy(buffer, &length, 2);
 	serializeFixed(buffer+4);
 	return this->bytes()+4;
 }
 
 template<class T>
-uint16_t vector<T>::deserializeFixed(uint8_t * buffer){
+uint16_t vector<T>::deserializeFixed(uint8_t * buffer)
+{
 	memcpy(data,buffer, this->bytes());
 	return this->bytes();
 }
+
 template<class T>
-uint16_t vector<T>::deserializeVariable(uint8_t * buffer){
+uint16_t vector<T>::deserializeVariable(uint8_t * buffer)
+{
 	uint32_t size;
 	memcpy(&size, buffer, 4);
 	setSize(size);
@@ -89,18 +126,20 @@ uint16_t vector<T>::deserializeVariable(uint8_t * buffer){
 	return this->bytes()+4;
 }
 
-
 template<class T>
-uint16_t vector<T>::serialize(uint8_t * buffer){
+uint16_t vector<T>::serialize(uint8_t * buffer)
+{
 	if (fixed) return serializeFixed(buffer);
 	else return this->serializeVariable(buffer);
 }
 
 template<class T>
-uint16_t vector<T>::deserialize(uint8_t* buffer){
+uint16_t vector<T>::deserialize(uint8_t* buffer)
+{
 	if (fixed) return deserializeFixed(buffer);
 	else return deserializeVariable(buffer);
 }
 
-}
+} /* namespace ROS */
+
 #endif /* VECTOR_H_ */
